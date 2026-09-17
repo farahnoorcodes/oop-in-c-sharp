c046e6200f43d6d69ff0e160f3708b982324af5b
Farah Noor <293658554+farahnoorcodes@users.noreply.github.com>
2026-09-17 09:55:29 +0500

    while loop

---
+++ 015.while_loop/while loop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace while_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1; // initialize counter variable
+
+            // loop continues as long as i is less than or equal to 5
+            while (i <= 5)
+            {
+                Console.WriteLine(i); // print the current value of i
+                i++; // increment i by 1 to move towards the loop's end condition
+            }
+
+            Console.ReadKey(); // wait for a key press before closing the console window
+        }
+    }
+}
