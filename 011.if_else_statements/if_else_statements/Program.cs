24369187e151c95ed5b34e3f71a269556d2fd7bd
Farah Noor <293658554+farahnoorcodes@users.noreply.github.com>
2026-09-17 09:12:25 +0500

    if else statements

---
+++ 011.if_else_statements/if_else_statements/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace if_else_statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int marks;
+            Console.Write("Enter marks: ");
+            marks = Convert.ToInt32(Console.ReadLine());
+            if(marks>=50) {
+                Console.WriteLine("Pass");
+            }
+            else {
+                Console.WriteLine("Fail");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 011.if_else_statements/if_else_statements/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("if_else_statements")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("if_else_statements")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("05b76253-c41d-48c3-bacc-45bec28fb44f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
