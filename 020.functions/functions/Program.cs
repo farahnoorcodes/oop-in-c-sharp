3e1015b13d73d04a810f274013f72551c03d01f8
Farah Noor <293658554+farahnoorcodes@users.noreply.github.com>
2026-09-17 16:45:01 +0500

    function

---
+++ 020.functions/functions/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace functions
+{
+    internal class Program
+    {
+        static void showmessage()
+        {
+            Console.WriteLine("welcome to OOP lab");
+        }
+        static void Main()
+        {
+            showmessage();
+            Console.ReadKey();
+        }
+    }
+}
