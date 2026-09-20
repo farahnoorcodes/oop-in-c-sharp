60ea4818f83e5eb7f1fb70e3faf9e2847af1f43c
Farah Noor <293658554+farahnoorcodes@users.noreply.github.com>
2026-09-20 21:42:41 +0500

    task3

---
+++ 23.task_3/task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace task_3//create a function named CheckNumber that takes an integer as input and returns a string indicating whether the number is positive, negative, or zero.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            class_for_task_3 obj = new class_for_task_3();
+            Console.WriteLine(obj.CheckNumber(5));
+            Console.WriteLine(obj.CheckNumber(-3));
+            Console.WriteLine(obj.CheckNumber(0));
+            Console.ReadKey();  
+        }
+    }
+}
---
+++ 23.task_3/task_3/class for task 3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace task_3
+{
+    internal class class_for_task_3
+    {
+        public string CheckNumber(int number)
+        {
+            if (number > 0)
+            {
+                return number+":  The number is positive.";
+            }
+            else if (number < 0)
+            {
+                return number+":  The number is negative.";
+            }
+            else
+            {
+                return number+":  The number is zero.";
+            }
+            
+        }
+    }
+}
