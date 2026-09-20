4388ab6b8ee6769b007af82411c881291a14095a
Farah Noor <293658554+farahnoorcodes@users.noreply.github.com>
2026-09-20 21:38:02 +0500

    task3

---
+++ 021.function_overloading_&_overriding/function overriding/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace function_overriding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            child obj = new child();
+            obj.show();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 021.function_overloading_&_overriding/function overriding/parent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace function_overriding
+{
+    internal class parent
+    {
+        public virtual void show()
+        {
+            Console.WriteLine(" Method of Parent Class");
+        }
+    }
+    class child : parent
+    {
+        public override void show()
+        {
+            Console.WriteLine(" Method of Child Class");
+        }
+    }
+
+}
