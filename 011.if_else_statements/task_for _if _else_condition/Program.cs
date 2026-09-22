794b3d2e5b5dc5a60a81bf72ad46aac3367eca28
Farah Noor <293658554+farahnoorcodes@users.noreply.github.com>
2026-09-22 19:39:11 +0500

    task for if_else conditions

---
+++ 011.if_else_statements/task_for _if _else_condition/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace task_for__if__else_condition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //write a c# program that marks from the user and displays the garde. use the following condtion:
+            //80 or above-->A
+            //70-79-->B
+            //60-69-->C
+            //50-59--D
+            //below 50-->F
+            int marks;
+            start:
+            Console.Write("Enter your marks: ");
+            marks = Convert.ToInt32(Console.ReadLine());
+
+            if (marks > 100 || marks < 0)
+            {
+                Console.WriteLine("Invalid marks entered");
+                goto start;
+            }
+            else if (marks >= 80)
+            {
+                Console.WriteLine("Your grade is A");
+            }
+            else if (marks >= 70)
+            {
+                Console.WriteLine("Your grade is B");
+            }
+            else if (marks >= 60)
+            {
+                Console.WriteLine("Your grade is C");
+            }
+            else if (marks >= 50)
+            {
+                Console.WriteLine("Your grade is D");
+            }
+            else
+            {
+                Console.WriteLine("Your grade is F");
+            }
+
+        }
+    }
+}
