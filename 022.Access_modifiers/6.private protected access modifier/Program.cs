cc739733cba6519f29ca8f211373c3b6c0971249
Farah Noor <293658554+farahnoorcodes@users.noreply.github.com>
2026-09-22 20:20:40 +0500

    private protected access modifier

---
+++ 022.Access_modifiers/6.private protected access modifier/Program.cs
@@ -0,0 +1,19 @@
+﻿using _6.private_protected_access_modifier;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        private_protected_class person1 = new private_protected_class();
+        person1.Display();
+
+        confidential person2 = new confidential();
+        person2.Show();
+
+        confidential person3 = new confidential("manahil", 19, "987654321234");
+        person3.Display();
+
+        confidential person4 = new confidential("emaan", 20, "12345678987654321");
+        person4.Display();
+    }
+}
---
+++ 022.Access_modifiers/6.private protected access modifier/private protected class.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace _6.private_protected_access_modifier
+{
+    internal class private_protected_class
+    {
+        private protected string name = "farah";
+        private protected int age = 18;
+        public void Display()
+        {
+            Console.WriteLine($"Name: {name}\n Age: {age}\n cnic: {cnic}");
+        }
+
+
+        private protected string cnic;
+    }
+
+    internal class confidential : private_protected_class
+    {
+        // Parameterless constructor (needed if you still want `new confidential()`)
+        public confidential() { }
+
+        // Parameterized constructor
+        public confidential(string a, int b, string c)
+        {
+            name = a;   // fixed: assign parameter INTO field
+            age = b; 
+            cnic = c;
+        }
+
+        public void Show()
+        {
+            name = "ehsan";
+            age = 20;
+            cnic = "1234565789";
+            Display();
+        }
+        
+    }
+
+    
+}
