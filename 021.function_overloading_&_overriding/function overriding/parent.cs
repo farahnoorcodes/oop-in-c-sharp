982932abc183b57f2a1a3b5de03eb9b557b63fa9
Farah Noor <293658554+farahnoorcodes@users.noreply.github.com>
2026-09-26 20:11:47 +0500

    function overriding

--- 021.function_overloading_&_overriding/function overriding/parent.cs
+++ 021.function_overloading_&_overriding/function overriding/parent.cs
@@ -3,14 +3,18 @@ namespace function_overriding
 {
     internal class parent
     {
-        public virtual void show()
+        public virtual void show()// "virtual" allows this method to be overridden by derived classes.
+                                  // Without "virtual", the child class could only hide it (using "new"),
+                                  // not truly override it.
         {
             Console.WriteLine(" Method of Parent Class");
         }
     }
     class child : parent
     {
-        public override void show()
+        public override void show()// "override" provides a new implementation of the parent's virtual method.
+                                   // At runtime, calling show() on a child object will always run THIS version,
+                                   // even if it's accessed through a parent-type reference (runtime polymorphism).
         {
             Console.WriteLine(" Method of Child Class");
         }
