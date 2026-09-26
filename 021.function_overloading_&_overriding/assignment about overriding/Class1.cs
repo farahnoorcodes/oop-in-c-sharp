6579bd73c97739dfbef6289478ea33a8a2b53fa2
Farah Noor <293658554+farahnoorcodes@users.noreply.github.com>
2026-09-26 20:03:15 +0500

    understanding overriding through assignment

---
+++ 021.function_overloading_&_overriding/assignment about overriding/Class1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+namespace assignment_about_overriding
+{
+    internal class parent
+    {
+        public virtual void display()
+        {
+            Console.WriteLine("This is parent class");
+        }
+        public virtual int add(int a, int b)
+        {
+            Console.Write("This is parent class add method: ");
+            return a + b;
+        }
+        public virtual long power(int a,int b)
+        {
+            int result = 1;
+            for(int i = 0; i < b; i++)
+            {
+                result *= a;
+            }
+            Console.Write($"In the parent class: {a}^{b} = ");
+            return result;
+        }
+        public virtual string concatenation(string a,string b)
+        {
+            Console.Write("In the parent class: ");
+            return a + b;
+        }
+        public virtual double roundoff(double a)
+        {
+            Console.WriteLine("In the parent class:");
+            Console.WriteLine($"the floor of {a} is = " + Math.Floor(a));
+            Console.Write($"{a} is Ceiling to = ");
+            return Math.Ceiling(a);
+        }
+    }
+
+    internal class child : parent
+    {
+        public override void display()
+        {
+            Console.WriteLine("This is child class");
+        }
+        public override int add(int a, int b)
+        {
+            Console.Write("This is child class add method: ");
+            return a + b ;
+        }
+        public override long power(int a, int b)
+        {
+            int result = 1;
+            for (int i = 0; i < b; i++)
+            {
+                result *= a;
+            }
+            Console.Write($"In the child class: {a}^{b} = ");
+            return result;
+        }
+        public override string concatenation(string a, string b)
+        {
+            Console.Write("In the child class: ");
+            return a + b;
+        }
+        public override double roundoff(double a)
+        {
+            Console.WriteLine("\nIn the child class:");
+            Console.WriteLine($"the floor of {a} is = " + Math.Floor(a));
+            Console.Write($"{a} is Ceiling to = ");
+            return Math.Ceiling(a);
+        }
+    }
+
+    internal class sub_child : child
+    {
+        public override void display()
+        {
+            Console.WriteLine("This is sub child class");
+        }
+        public override int add(int a, int b)
+        {
+            Console.Write("This is sub child class add method: ");
+            return a + b ;
+        }
+        public override long power(int a, int b)
+        {
+            int result = 1;
+            for (int i = 0; i < b; i++)
+            {
+                result *= a;
+            }
+            Console.Write($"In the sub child class: {a}^{b} = ");
+            return result;
+        }
+        public override string concatenation(string a, string b)
+        {
+            Console.Write("In the sub child class: ");
+            return a + b;
+        }
+        public override double roundoff(double a)
+        {
+            Console.WriteLine("\nIn the sub child class:");
+            Console.WriteLine($"the floor of {a} is = " + Math.Floor(a));
+            Console.Write($"{a} is Ceiling to = ");
+            return Math.Ceiling(a);
+        }
+    }
+
+    internal class sub_sub_child : sub_child
+    {
+        public override void display()
+        {
+            Console.WriteLine("This is sub sub child class");
+        }
+        public override int add(int a, int b)
+        {
+            Console.Write("This is sub sub child class add method: ");
+            return a + b ;
+        }
+        public override long power(int a, int b)
+        {
+            int result = 1;
+            for (int i = 0; i < b; i++)
+            {
+                result *= a;
+            }
+            Console.Write($"In the  sub sub child class: {a}^{b} = ");
+            return result;
+        }
+        public override string concatenation(string a, string b)
+        {
+            Console.Write("In the sub sub child class: ");
+            return a + b;
+        }
+        public override double roundoff(double a)
+        {
+            Console.WriteLine("\nIn the sub sub child class:");
+            Console.WriteLine($"the floor of {a} is = " + Math.Floor(a));
+            Console.Write($"{a} is ceiling to = ");
+            return Math.Ceiling(a);
+        }
+    }
+}
---
+++ 021.function_overloading_&_overriding/assignment about overriding/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+namespace assignment_about_overriding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\noverrided display function\n");
+
+            parent display = new parent();
+            display.display();
+
+            child display2 = new child();
+            display2.display();
+
+            sub_child display3 = new sub_child();
+            display3.display();
+
+            sub_sub_child display4 = new sub_sub_child();
+            display4.display();
+
+            Console.WriteLine("\noverrideed add function\n");
+
+            parent parentAdd = new parent();
+            Console.WriteLine(parentAdd.add(23, 56));
+
+            child childAdd = new child();
+            Console.WriteLine(childAdd.add(45, 56));
+
+            sub_child subChildAdd = new sub_child();
+            Console.WriteLine(subChildAdd.add(4, 78));
+
+            sub_sub_child subSubChildAdd = new sub_sub_child();
+            Console.WriteLine(subSubChildAdd.add(58, 67));
+
+            Console.WriteLine("\noverrided power method\n");
+
+            parent parentPower = new parent();
+            Console.WriteLine(parentPower.power(4, 7));
+
+            child childpower = new child();
+            Console.WriteLine(childpower.power(7, 3));
+
+            sub_child subchildpower = new sub_child();
+            Console.WriteLine(subchildpower.power(2, 6));
+
+            sub_sub_child subsubchildpower = new sub_sub_child();
+            Console.WriteLine(subsubchildpower.power(9, 4));
+
+            Console.WriteLine("\noverrided concatenation method\n");
+
+            parent con1 = new parent();
+            Console.WriteLine(con1.concatenation("Farah", "Noor"));
+
+            child con2 = new child();
+            Console.WriteLine(con2.concatenation("Computer ", "Engineering"));
+
+            sub_child con3 = new sub_child();
+            Console.WriteLine(con3.concatenation("Roll No: ", "18-CPE-2025"));
+
+            sub_sub_child con4 = new sub_sub_child();
+            Console.WriteLine(con4.concatenation("Batch: ", "2k25"));
+
+            Console.WriteLine("\noverrided roundoff method\n");
+
+            parent rf1 = new parent();
+            Console.WriteLine(rf1.roundoff(3.687));
+
+            child rf2 = new child();
+            Console.WriteLine(rf2.roundoff(2.34));
+
+            sub_child rf3 = new sub_child();
+            Console.WriteLine(rf3.roundoff(567.555));
+
+            sub_sub_child rf4 = new sub_sub_child();
+            Console.WriteLine(rf4.roundoff(65.32));
+        }
+    }
+}
